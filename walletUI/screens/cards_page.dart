462d914f2f683b85bc6bb7f91bf80997db074d24
eddy <106661971+eddiemws@users.noreply.github.com>
2024-11-12 23:08:27 +0300

    Update cards_page.dart

--- walletUI/screens/cards_page.dart
+++ walletUI/screens/cards_page.dart
@@ -87,7 +87,9 @@ class CardPage extends StatelessWidget {
                                 color: Colors.blue),
                           ),
                         ),
-                       
+                        const SizedBox(
+                          width: 16,
+                        ),
                         Container(
                           decoration: BoxDecoration(
                               color: Colors.white,
